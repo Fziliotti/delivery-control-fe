4db6e20a75b1cd7781e4217d05d256521b9c5a53
Fabricio laptop <fabricio.ziliotti@flowls.com>
2024-02-01 10:32:59 -0300

    Setup project with shadcn and tailwindcss

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import path from "node:path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // Configuracao do vite, para que quando fazer o build, as importações com @ sejam entendidas.
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
+})
