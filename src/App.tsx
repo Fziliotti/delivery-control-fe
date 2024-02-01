9474e2ce106fd8d28bbd32cfc598fad0bf79f4d4
Fabricio laptop <fabricio.ziliotti@flowls.com>
2024-02-01 11:09:44 -0300

    feature: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { Button } from './components/ui/button'
 import './global.css'
 
+import { RouterProvider } from 'react-router-dom'
+
+import { Router } from './routes'
+
 export function App() {
-  return (
-    <>
-      <div className="flex h-screen items-center justify-center">
-        <Button>Enviar</Button>
-      </div>
-    </>
-  )
+  return <RouterProvider router={Router}></RouterProvider>
 }
