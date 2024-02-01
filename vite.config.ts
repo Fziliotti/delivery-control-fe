3769817eabb0f041d8d9117d4f50d8ae6347ffd0
Fabricio laptop <fabricio.ziliotti@flowls.com>
2024-02-01 10:57:42 -0300

    chore: add prettier/eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from "node:path"
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -8,7 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       // Configuracao do vite, para que quando fazer o build, as importações com @ sejam entendidas.
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
